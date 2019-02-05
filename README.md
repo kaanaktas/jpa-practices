4b2afb579ed123f3d416c86ce0b22202659ea1ff
aktaskaan@yahoo.com <aktaskaan@yahoo.com>
2019-02-05 18:40:33 +0000

    Added travis.yml

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # jpa-practices
 
-This project depicts the Jpa Example with Hibernate using H2. It contains all relationships and simplified fundamentals of JPA Concepts.     
+This project depicts the Jpa Example with Hibernate using H2. It contains all relationships and simplified fundamentals of JPA Concepts.  
+
+[![Build Status](https://travis-ci.org/kaanaktas/jpa-practices.svg?branch=master)](https://travis-ci.org/kaanaktas/jpa-practices)
+   
---
+++ src/test/java/com/sps/service/JpaServiceTest.java
@@ -0,0 +1,68 @@
+package com.sps.service;
+
+import com.sps.config.JPAUtil;
+import com.sps.entity.Employee;
+import org.junit.*;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by kaktas on 05-Feb-19
+ */
+
+public class JpaServiceTest {
+
+    private static EntityManager entityManager = null;
+    private static JpaService jpaService = null;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        JPAUtil.disableHibernateSqlTrace();
+        entityManager = JPAUtil.getEntityManagerFactory().createEntityManager();
+        jpaService = new JpaService(entityManager);
+        jpaService.processBulkData();
+    }
+
+    @Test
+    public void TestFindAllDepartmentsWithEmployees(){
+        JPAUtil.enableHibernateSqlTrace();
+        jpaService.findAllDepartmentsWithEmployees();
+        JPAUtil.disableHibernateSqlTrace();
+    }
+
+    @Test
+    public void TestRemoveDepartment(){
+        JPAUtil.enableHibernateSqlTrace();
+        jpaService.removeDepartmentById(1);
+        JPAUtil.disableHibernateSqlTrace();
+    }
+
+    @Test
+    public void TestRemoveEmployee(){
+        JPAUtil.enableHibernateSqlTrace();
+        jpaService.removeEmployeeByDepartmentAndById(2, 2);
+        JPAUtil.disableHibernateSqlTrace();
+     }
+
+     @Test
+     public void TestFindEmployeeByCriteriaByName(){
+         JPAUtil.enableHibernateSqlTrace();
+         Employee employee = jpaService.findEmployeeByCriteriaByName("Timothee Stuckey");
+         System.out.println("Employee Full Name:"+employee.getName()+" Employee Department:"+employee.getDepartment().getName());
+         JPAUtil.disableHibernateSqlTrace();
+     }
+
+    @Test
+    public void TestNativeQueries(){
+        JPAUtil.enableHibernateSqlTrace();
+        jpaService.nativeQueries();
+        JPAUtil.disableHibernateSqlTrace();
+    }
+
+
+    @AfterClass
+    public static void tearDownClass(){
+        entityManager.close();
+        JPAUtil.shutdown();
+    }
+}
